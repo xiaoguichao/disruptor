d149b284971805c8d9ed469b470f84e093c4c0db
github-action <github-action@github.com>
2021-07-13 10:22:15 +0000

    Simple GitHub Pages Deploy Action

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.lmax.disruptor {
-    requires jdk.unsupported; // Temporarily required for sum.misc.Unsafe until Unsafe is removed
-
-    exports com.lmax.disruptor;
-    exports com.lmax.disruptor.dsl;
-    exports com.lmax.disruptor.util;
-}
